cb650282f13033c6f907b3344c91d67978c958a9
Alex Pratte <alexpratte16@gmail.com>
2021-09-29 21:25:33 -0400

    software only sends MIDI packets on pin status changes

--- Software/MIDI_keyboard/MIDI_keyboard.ino
+++ Software/MIDI_keyboard/MIDI_keyboard.ino
@@ -1,60 +1,104 @@
 #include <USB-MIDI.h>
-#include <frequencyToNote.h>
+#include <pitchToNote.h>
 USBMIDI_CREATE_DEFAULT_INSTANCE();
 
-unsigned long t0 = millis();
-unsigned long tClock = millis();
+
 
 using namespace MIDI_NAMESPACE;
 
 
-#define COL_START 2
-#define COL_END 7
-#define ROW_START 8
-#define ROW_END 15
+#define COL_START 0
+#define COL_END 5
+#define ROW_START 6
+#define ROW_END 11
 #define COLS_PER_ROW 6
+
+#define LEFT_START_ROW 6
+#define RIGHT_START_ROW 8
 
 #define NUM_KEYS 24
 
+#define VELOCITY 64
+#define CHANNEL 1
+
+#define SCAN_RATE_MS 5
 
 uint8_t keyPressed[NUM_KEYS];
+unsigned long t0 = millis();
+uint8_t pinMap[12]; //I did not read the data sheet carefully enough so now I need a pin map
 
 
 
 
 
-
-int scanKey(int row, uint8_t baseNote){
+int scanRow(int row, uint8_t baseNote, uint8_t* distance){
+  
   for(int iCol = COL_START; iCol <= COL_END; iCol++){
-    digitalWrite(iCol, 0);
-    uint8_t checkPressed = ~digitalRead(row);
-    if(checkPressed != keyPressed[((row-ROW_START)*COLS_PER_ROW)+iCol]){
+    digitalWrite(pinMap[iCol], 0);
+    uint8_t checkPressed = !digitalRead(pinMap[row]);
+    /*Serial.print(checkPressed);
+    Serial.print("\n");*/
+    uint8_t idx = ((row-ROW_START)*COLS_PER_ROW)+(iCol-COL_START);
+    if(checkPressed != keyPressed[idx]){
+      //Serial.print(idx);
+      //Serial.print(" changed!\n");
+      
       if(checkPressed){
         //note on baseNote + distance from base
+        MIDI.sendNoteOn(baseNote+(*distance), VELOCITY, CHANNEL);
       }
       else{
-        //note off baseNote + distance from 
+        MIDI.sendNoteOff(baseNote+(*distance), VELOCITY, CHANNEL);
       }
-      keyPressed[((row-ROW_START)*COLS_PER_ROW)+iCol] ^= 1;
+      keyPressed[idx] = !keyPressed[idx];
 
     }
-    digitalWrite(iCol, 1);
+    digitalWrite(pinMap[iCol], 1);
+    (*distance)++;
   }
 }
   
 
 void setup() {
-  // put your setup code here, to run once:
+  //Serial IO
+  Serial.begin(115200);
+  while (!Serial);
+  MIDI.begin(CHANNEL);  
+  
+  //set up pin map
+  for(int iPin = 0; iPin < sizeof(pinMap)/sizeof(uint8_t); iPin++){
+    pinMap[iPin] = iPin+2;
+  }
+  pinMap[9] = 16;
+  pinMap[10] = 14;
+  pinMap[11] = 15;
+
+  //set up pins
   for(int iRow = ROW_START; iRow <= ROW_END; iRow++){
-    pinMode(iRow, INPUT_PULLUP);
+    pinMode(pinMap[iRow], INPUT_PULLUP);
   }
   for(int iCol = COL_START; iCol <= COL_END; iCol++){
-    pinMode(iCol, OUTPUT);
-    digitalWrite(iCol, 1);
+    pinMode(pinMap[iCol], OUTPUT);
+    digitalWrite(pinMap[iCol], 1);
   }
+
+  Serial.print("MIDI_keyboard V1 online\n");
+
 }
 
 void loop() {
-  uint8_t leftHandBaseNote;
-  uint8_t rightHandBaseNote;
+  if(millis()-t0 > SCAN_RATE_MS){
+    t0 = millis();
+    uint8_t leftBaseNote = pitchC1;
+    uint8_t rightBaseNote = pitchC2;
+    uint8_t leftCounter = 0;
+    uint8_t rightCounter = 0;
+    
+    for(int iRow = LEFT_START_ROW; iRow < LEFT_START_ROW+2; iRow++){
+      scanRow(iRow, leftBaseNote, &leftCounter);
+    }
+    for(int iRow = RIGHT_START_ROW; iRow < RIGHT_START_ROW+2; iRow++){
+      scanRow(iRow, rightBaseNote, &rightCounter);
+    }
+  }
 }
