c2a71bf1aa2a31c7805d8c84ae843075a110dbb1
Alex Pratte <alexpratte16@gmail.com>
2022-05-29 14:46:01 -0400

    Added change control support

--- Software/MIDI_keyboard/MIDI_keyboard.ino
+++ Software/MIDI_keyboard/MIDI_keyboard.ino
@@ -7,23 +7,21 @@ USBMIDI_CREATE_DEFAULT_INSTANCE();
 using namespace MIDI_NAMESPACE;
 
 
-#define COL_START 0
-#define COL_END 5
-#define ROW_START 6
-#define ROW_END 11
-#define COLS_PER_ROW 6
-
-#define LEFT_START_ROW 6
+#define COL_START       0
+#define COL_END         5 
+#define ROW_START       6
+#define ROW_END         11
+#define COLS_PER_ROW    6 
+#define LEFT_START_ROW  6
 #define RIGHT_START_ROW 8
-
-#define NUM_KEYS 24
-
-#define VELOCITY 64
-#define CHANNEL 1
-
-#define SCAN_RATE_MS 5
+#define NUM_KEYS        24
+#define VELOCITY        64
+#define CHANNEL         1
+#define SCAN_RATE_MS    5
+#define NUM_POTS        4
 
 uint8_t keyPressed[NUM_KEYS];
+uint8_t potVals[NUM_POTS];
 unsigned long t0 = millis();
 uint8_t pinMap[12]; //I did not read the data sheet carefully enough so now I need a pin map
 
@@ -31,18 +29,13 @@ uint8_t pinMap[12]; //I did not read the data sheet carefully enough so now I ne
 
 
 
-int scanRow(int row, uint8_t baseNote, uint8_t* distance){
+void scanRow(int row, uint8_t baseNote, uint8_t* distance){
   
   for(int iCol = COL_START; iCol <= COL_END; iCol++){
     digitalWrite(pinMap[iCol], 0);
     uint8_t checkPressed = !digitalRead(pinMap[row]);
-    /*Serial.print(checkPressed);
-    Serial.print("\n");*/
     uint8_t idx = ((row-ROW_START)*COLS_PER_ROW)+(iCol-COL_START);
     if(checkPressed != keyPressed[idx]){
-      //Serial.print(idx);
-      //Serial.print(" changed!\n");
-      
       if(checkPressed){
         //note on baseNote + distance from base
         MIDI.sendNoteOn(baseNote+(*distance), VELOCITY, CHANNEL);
@@ -57,12 +50,23 @@ int scanRow(int row, uint8_t baseNote, uint8_t* distance){
     (*distance)++;
   }
 }
-  
+
+void scanPots(){
+  for(int iPot = 0; iPot < NUM_POTS; iPot++){
+    uint8_t potVal = 127 - (uint8_t)(analogRead(A0+iPot) >> 3);
+    
+    if(potVal !=  potVals[iPot]){
+      potVals[iPot] = potVal;
+      MIDI.sendControlChange(iPot, potVal, CHANNEL);
+    }
+  }
+
+}
 
 void setup() {
   //Serial IO
   Serial.begin(115200);
-  while (!Serial);
+  //while (!Serial);
   MIDI.begin(CHANNEL);  
   
   //set up pin map
@@ -82,7 +86,9 @@ void setup() {
     digitalWrite(pinMap[iCol], 1);
   }
 
-  Serial.print("MIDI_keyboard V1 online\n");
+  for(int iPot = 0; iPot < NUM_POTS; iPot++){
+    potVals[iPot] = 0;
+  }
 
 }
 
@@ -93,6 +99,9 @@ void loop() {
     uint8_t rightBaseNote = pitchC2;
     uint8_t leftCounter = 0;
     uint8_t rightCounter = 0;
+
+
+    scanPots();
     
     for(int iRow = LEFT_START_ROW; iRow < LEFT_START_ROW+2; iRow++){
       scanRow(iRow, leftBaseNote, &leftCounter);
